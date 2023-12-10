5dd76a9ea24d2b66e058a3f833ec66496ed6cc10
ramdhanyA <ariqathallah38@gmail.com>
2023-12-10 14:40:09 +0900

    added touroku form

--- app/benefit.tsx
+++ app/benefit.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
 
-export default function Benefit({ heading, description, className }: { heading: string, description?: string, className?: string }) {
+export default function Benefit({ heading, description, className, src }: { heading: string, description?: string, className?: string, src: string }) {
 	if (!description) {
 		description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eaque soluta vero, saepe nihil nostrum sapiente! Odio quae rem tempora voluptatum fugiat accusantium animi."
 	}
 	return (
-		<div className={className + " text-center max-w-[350px]"}>
-			<Image src="./hero.svg" alt="Logo" width={500} height={500}></Image>
-			<h1 className="text-4xl font-rampart h-[150px]">{heading}</h1>
+		<div className={className + " text-center max-w-[350px] mx-auto"}>
+			<div className="h-[230px] mx-auto">
+				<Image src={src} alt="Logo" width={300} height={300}></Image>
+			</div>
+			<h1 className="text-3xl font-rampart h-[110px]">{heading}</h1>
 			<p className="text-lg font-noto text-white">{description}</p>
 		</div>
 	)
--- app/page.tsx
+++ app/page.tsx
@@ -3,51 +3,59 @@ import Benefit from "./benefit";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <Image
-        className="light:brightness-0 p-4"
-        src="./kikkacode.svg"
-        alt="Logo"
-        width={200}
-        height={200}
-      ></Image>
-      <section id="hero" className="flex">
-        <div id="hero-text" className="ml-16 mt-16 w-[45%] dark:text-white">
-          <h1 className="font-rampart text-6xl">
-            <span className="text-blue">冬休み</span>を
-            <span className="text-red">きっかけ</span>に、
-            <span className="text-green">プログラム</span>を作り
-            <span className="text-yellow">始めよう</span>。
-          </h1>
-          <p className="font-noto mt-5 w-[75%] text-xl leading-7">
-            プログラミング言語というのは身近なものにも役に立つことを知っていますか？授業で学んだことをアプリにしよう！
-          </p>
-        </div>
-        <div id="hero-image" className="m-auto flex w-[55%] justify-center">
-          <Image src="./hero.svg" alt="Logo" width={500} height={500}></Image>
-        </div>
-      </section>
-      <section
-        id="benefits"
-        className="m-20 flex justify-center gap-16 dark:text-white"
-      >
-        <Benefit
-          className="text-blue"
-          heading="JASSOにいるうちに、完全なプロジェクトを作らない？"
-          description="進学したら先生やクラスメートに見せるものがあるように、このイベントをきっかけにプロジェクトを始めることができる。"
-        />
-        <Benefit
-          className="text-red"
-          heading="実用的なコードはどう学べばいいの？"
-          description="理論的な授業が良い参考にはなれますが、どうやって実用するか見てみましょう！"
-        />
-        <Benefit
-          className="text-yellow"
-          heading="この学校に同じ興味を持つ人がいるかな？"
-          description="プロジェクトを始めたいと思っても、誰かと一緒にする人がいればいいんじゃない？このイベントに参加してみたら、他の同じ興味を持つ人と会うことができる。"
-        />
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<Image
+				className="light:brightness-0 p-4"
+				src="./kikkacode.svg"
+				alt="Logo"
+				width={200}
+				height={200}
+			></Image>
+			<section id="hero" className="flex">
+				<div id="hero-text" className="ml-16 mt-16 w-[45%] dark:text-white">
+					<h1 className="font-rampart text-6xl">
+						<span className="text-blue">冬休み</span>を
+						<span className="text-red">きっかけ</span>に、
+						<span className="text-green">プログラム</span>を作り
+						<span className="text-yellow">始めよう</span>。
+					</h1>
+					<p className="font-noto mt-5 w-[75%] text-xl leading-7">
+						プログラミング言語というのは身近なものにも役に立つことを知っていますか？授業で学んだことをアプリにしよう！
+					</p>
+				</div>
+				<div id="hero-image" className="m-auto flex w-[55%] justify-center">
+					<Image src="./hero.svg" alt="Logo" width={500} height={500}></Image>
+				</div>
+			</section>
+			<section
+				id="benefits"
+				className="m-10 flex justify-center gap-20 dark:text-white"
+			>
+				<Benefit
+					className="text-blue"
+					src="/benefit-1.png"
+					heading="JASSOにいるうちに、完全なプロジェクトを作らない？"
+					description="進学したら先生やクラスメートに見せるものがあるように、このイベントをきっかけにプロジェクトを始めることができる。"
+				/>
+				<Benefit
+					className="text-red"
+					src="/benefit-2.png"
+					heading="実用的なコードはどう学べばいいの？"
+					description="理論的な授業が良い参考にはなれますが、どうやって実用するか見てみましょう！"
+				/>
+				<Benefit
+					className="text-yellow"
+					src="/benefit-2.png"
+					heading="この学校に同じ興味を持つ人がいるかな？"
+					description="プロジェクトを始めたいと思っても、誰かと一緒にする人がいればいいんじゃない？このイベントに参加してみたら、他の同じ興味を持つ人と会うことができる。"
+				/>
+			</section>
+			<section id="registration" className="flex justify-center mb-20">
+				<button className=" bg-blue border-2 rounded-xl border-red p-2 hover:bg-yellow">
+					登録フォームへ
+				</button>
+			</section>
+		</main>
+	);
 }
